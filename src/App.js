47f71bd848c21ff476283a2eef7064e160bc9f46
Brian Powers <bpowers@dmsi.com>
2021-04-14 14:08:55 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          background: "hsl(195deg, 20%, 86%)",
+          position: "fixed",
+          height: "40%",
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+      </div>
       <CharacterEditor />
       <Footer />
     </>
